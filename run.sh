1de5f7f41bd2e4ca30fca8b31e208495b52317b3
Ellenszenves <fallouterdo@gmail.com>
2022-07-14 16:30:21 +0200

    javítás

--- run.sh
+++ run.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
+download() {
+if [[ -f "./AdventureWorks2019.bak" ]]
+then
+echo "Fájl már letöltve!"
+else
 wget "https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2019.bak"
+fi
+main
+}
 
 run-docker() {
 docker build -t advan:1 .
 }
 
 main() {
-  read -p "Építsük az image-et?(i/n)" func
-  if [[ $func == "i" ]]
+  read -p "Indulhat a letöltés?(i/n)" fun
+  if [[ $fun == "i" ]]
   then
-  run-docker
+  download
   else
-  exit
+    read -p "Építsük az image-et?(i/n)" func
+    if [[ $func == "i" ]]
+    then
+    run-docker
+    else
+    exit
+    fi
   fi
 }
 main
