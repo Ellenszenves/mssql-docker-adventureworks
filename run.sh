16bd2aca1b17762c2fd5c2e9735dad439375435c
Ellenszenves <88683249+Ellenszenves@users.noreply.github.com>
2022-07-14 16:16:09 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,15 @@
 #!bin/bash
 wget "https://github.com/Microsoft/sql-server-samples/releases/download/adventureworks/AdventureWorks2019.bak"
+main
+run-docker() {
+docker build -t advan:1 .
+}
+main() {
+  read -p "Építsük az image-et?" func
+  if [[ $func == "i" ]]
+  then
+  run-docker
+  else
+  exit
+  fi
+}
